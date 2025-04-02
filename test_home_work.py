e9890050606242d43cea1839ba5ee53b836d0f10
Ramin Safarov <silent1962@gmail.com>
2025-04-02 22:49:01 +0300

    Lesson-6: Домашнее задание

--- test_home_work.py
+++ test_home_work.py
@@ -98,3 +98,4 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = print_name_function(find_registration_button_on_login_page, page_url + ', ' +button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
